e3c5e5b1bb49af8395b6911aafb3f69e7c926e34
MatheusMarcelo01 <matheusmpmenezes@protonmail.com>
2024-10-11 14:16:52 -0300

    ok

--- pages/api/products/index.tsx
+++ pages/api/products/index.tsx
@@ -1,8 +1,8 @@
-
 // pages/api/products/index.tsx
+import { NextApiRequest, NextApiResponse } from 'next';
 import pool from '../../../utils/db';
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { name, image, price } = req.body;
 
